2a6100783ed69afa739152d7d1a6195c0962d9b5
Abu Jakaria <abujakaria316@gmail.com>
2024-10-17 16:56:05 +0600

    sum of 1-n using math

--- sumofN.c
+++ sumofN.c
@@ -10,11 +10,21 @@ int main()
     (1≤N≤109). That means I'm adding 10^9 numbers together and its getting bigger when I'm adding them togerther. example: If I add 10^9 + 10^9 it will be 10^18 that int can't afford/store. thats why we took long long for that.
      */
 
+    /* For this program I've [time limit per test 0.25 seconds
+    memory limit per test 256 megabytes] this condition. But I can iterate 10^8 per mili seconds that's why I can't solve this using loop/traverse. I have to use mathmatical operation for that.
+     */
+
     long long sum = 0;
-    for (int i = 1; i <= n; i++)
-    {
-        sum += i;
-    }
+
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     sum += i;
+    // }
+    // printf("%lld", sum);
+
+    sum = (1LL * n * (n + 1)) / 2;
+
     printf("%lld", sum);
+
     return 0;
 }
