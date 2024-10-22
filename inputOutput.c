71047b5f447023a75a76d14d843d5c9b781514f5
Abu Jakaria <abujakaria316@gmail.com>
2024-10-22 06:38:18 +0600

    string

---
+++ inputOutput.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // string array (Separate charecter)
+    char str[10] = {'a', 'b', 'c', 'd'};
+
+    // string input
+    char str2[10] = "abcdefgh";
+
+    printf("%s", str2);
+    return 0;
+}
