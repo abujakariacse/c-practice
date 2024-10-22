4fb4231a6f9970405195b09f28d96c3057ecd6cd
Abu Jakaria <abujakaria316@gmail.com>
2024-10-22 06:43:29 +0600

    str input

--- inputOutput.c
+++ inputOutput.c
@@ -2,12 +2,18 @@
 
 int main()
 {
+    // If we give string input more then the length of the string array it will overflow and abort the program
+
     // string array (Separate charecter)
     char str[10] = {'a', 'b', 'c', 'd'};
 
     // string input
     char str2[10] = "abcdefgh";
 
-    printf("%s", str2);
+    // string input
+    char str3[10];
+    scanf("%s", str3);
+
+    printf("%s", str3);
     return 0;
 }
